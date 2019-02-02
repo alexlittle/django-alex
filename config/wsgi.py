64ecf3e4ce2591217f969fb9098c9fa23467459a
Alex Little <alex@digital-campus.org>
2019-02-02 16:43:42 +0200

    fix wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mpowering project.
+WSGI config for project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from django.core.wsgi import get_wsgi_application
 
-from django.core import wsgi
-application = wsgi.get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+application = get_wsgi_application()
