4d3f0faf410bbaf616fb5552350e3cd988391128
Alex Little <alex@digital-campus.org>
2020-02-28 12:06:52 +0100

    add news feed

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
 from blog import views as blog_views
+from blog.feeds import LatestNews
 
 urlpatterns = [
     url(r'^$', blog_views.home_view, name="blog_home"),
+    url(r'^feed/$', LatestNews(), name="blog_feed"),
     url(r'^(?P<blog_slug>\w[\w/-]*)$', blog_views.blog_view, name="blog_article"),
+    
 ]
