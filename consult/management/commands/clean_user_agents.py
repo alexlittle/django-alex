7d3ed20d9a16c3ccbd3d181d6e8e54eb28b1b5e9
Alex Little <consult@alexlittle.net>
2023-02-28 20:17:38 +0200

    process in blocks

--- consult/management/commands/clean_user_agents.py
+++ consult/management/commands/clean_user_agents.py
@@ -3,6 +3,8 @@
 Management command to remove bots/crawlers from trackers
 """
 
+import math
+
 from django.core.management.base import BaseCommand
 
 from consult.models import Tracker
@@ -13,8 +15,16 @@ class Command(BaseCommand):
     help = "Removes bots/crawlers from trackers"
 
     def handle(self, *args, **options):
-        rts = Tracker.objects.all()
-        for rt in rts:
-            if search_crawler.is_search_crawler(rt.agent):
-                print("found: " + rt.agent)
-                rt.delete()
+        total_count = Tracker.objects.all().count()
+        blocks = math.ceil(total_count/10000)
+        
+        for i in range(0, blocks):
+            start = i*10000
+            count = 0
+            trackers = Tracker.objects.all()[start:10000]
+            for tracker in trackers:
+                if search_crawler.is_search_crawler(tracker.agent):
+                    print("found: " + tracker.agent)
+                    tracker.delete()
+                    count += 1
+            print("Deleted %d in range %d to %d" % (count, start, start+10000))
