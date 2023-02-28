719cd1f7532286303808383411e45c702db186f4
Alex Little <consult@alexlittle.net>
2023-02-28 18:17:28 +0200

    update

--- consult/management/commands/clean_user_agents.py
+++ consult/management/commands/clean_user_agents.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
     help = "Removes bots/crawlers from trackers"
 
     def handle(self, *args, **options):
-        rts = Tracker.objects.all()[:10000]
+        rts = Tracker.objects.all()[:100000]
         for rt in rts:
             if search_crawler.is_search_crawler(rt.agent):
                 print("found: " + rt.agent)
