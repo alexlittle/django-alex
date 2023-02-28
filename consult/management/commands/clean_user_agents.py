c28c026f31c3a12a6037889026c6ce350214b75e
Alex Little <consult@alexlittle.net>
2023-02-28 20:25:21 +0200

    fix blocks

--- consult/management/commands/clean_user_agents.py
+++ consult/management/commands/clean_user_agents.py
@@ -13,18 +13,21 @@ from consult.lib import search_crawler
 
 class Command(BaseCommand):
     help = "Removes bots/crawlers from trackers"
-
+    block_size = 5000
+    
     def handle(self, *args, **options):
         total_count = Tracker.objects.all().count()
-        blocks = math.ceil(total_count/10000)
+        blocks = math.ceil(total_count/self.block_size)
         
-        for i in range(0, blocks):
-            start = i*10000
+        for i in range(0, blocks+1):
+            start = i*self.block_size
+            end = start+self.block_size
             count = 0
-            trackers = Tracker.objects.all()[start:10000]
+            trackers = Tracker.objects.all()[start:end]
+            print("checking %d trackers" % trackers.count())
             for tracker in trackers:
                 if search_crawler.is_search_crawler(tracker.agent):
                     print("found: " + tracker.agent)
                     tracker.delete()
                     count += 1
-            print("Deleted %d in range %d to %d" % (count, start, start+10000))
+            print("Deleted %d in range %d to %d" % (count, start, end))
