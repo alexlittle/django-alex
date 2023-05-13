1cdd532bdef57cd377933c85500a6fa1d772b7c2
Alex Little <consult@alexlittle.net>
2023-05-13 07:33:37 +0300

    fixes

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+from django.conf.urls.static import static
 
 if settings.BLOG_ENABLED:
     urlpatterns = [
@@ -13,3 +14,6 @@ else:
         path('admin/', admin.site.urls),
         path('', include('consult.urls')),
     ]
+    
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
